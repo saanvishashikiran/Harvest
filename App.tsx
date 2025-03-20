f72b254c2b723706b7e8b7bac9cf0589260dff5d
Nadean Abduljelil <nadeanmabdul@gmail.com>
2025-03-20 11:54:13 -0400

    Candidates feed initial

--- App.tsx
+++ App.tsx
@@ -1,15 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View , ScrollView, SafeAreaView} from 'react-native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import AppLoading from 'expo-app-loading';
+import CandidatesFeed from './screens/CadidatesFeed';
 export default function App() {
+
+  let [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <CandidatesFeed />  
       <StatusBar style="auto" />
     </View>
+
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
