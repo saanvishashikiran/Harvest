ccd52a845dbf1b1107714adfc5e7e57b74e0c8e5
Grace <rainbowponysun@gmail.com>
2025-03-24 12:06:40 -0400

    Adding Jobs, Active Posts, and My Jobs pages

--- App.tsx
+++ App.tsx
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
+import { TextInput } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import Tabs from "./components/Tabs"
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tabs />
+    </NavigationContainer>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
---
+++ components/AddPage.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View, Image } from 'react-native';
+import React from "react"
+
+const AddPage = () => {
+    return (
+        <View>
+            <View style={{marginTop: 250, alignItems: "center"}}>
+                <Text>This is the Add Page</Text>
+             </View>
+        </View>
+    )
+}
+
+export default AddPage;
+
+const styles = StyleSheet.create({})
